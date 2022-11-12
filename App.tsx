6d5185c985e7722dbebdeccacb7277c19095c6a9
Alexander Babak <bbkshow1987@gmail.com>
2022-11-12 11:20:33 +0200

    install navigation

--- App.tsx
+++ App.tsx
@@ -5,16 +5,7 @@ import {NativeBaseProvider, Box} from 'native-base';
 export const App = () => {
   return (
     <NativeBaseProvider>
-      <Box>Hello world</Box>
+      <Box>Babak connection</Box>
     </NativeBaseProvider>
   );
 };
-
-const styles = StyleSheet.create({
-  view: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  text: {fontSize: 20, fontWeight: '700'},
-});
