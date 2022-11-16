9e577437d7e5c45378c067ac3e72dde917be1916
Alexander Babak <bbkshow1987@gmail.com>
2022-11-16 10:10:58 +0200

    add splashscreen

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { theme } from './assets/theme/theme';
 import { NativeBaseProvider } from 'native-base';
 import { NavigationContainer } from '@react-navigation/native';
 import { StackNavigation } from './navigation/stack-navigation/stack-navigation';
+import SplashScreen from 'react-native-splash-screen';
 
 export const App = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
   return (
     <>
       <NativeBaseProvider theme={theme}>
